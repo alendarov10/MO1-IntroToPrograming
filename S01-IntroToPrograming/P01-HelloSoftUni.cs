45d67aa64180b538a82977e97bd02d92530722ce
Ibrahim Alendar <alendarov_10@abv.bg>
2025-11-26 21:59:33 +0200

    code

---
+++ S01-IntroToPrograming/P01-HelloSoftUni.cs
@@ -0,0 +1,9 @@
+using System;
+//Ctrl+K+D
+public class Program
+{
+	public static void Main()
+	{
+		Console.Write("Hello, World!");
+	}
+}
---
+++ S01-IntroToPrograming/P02-Expression.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write('*');
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
