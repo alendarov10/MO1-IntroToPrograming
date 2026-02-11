3a730f4753640b1e67ca2ff3cee4613d62db668c
Ibrahim Alendar <alendarov_10@abv.bg>
2026-02-11 20:48:00 +0200

    project

---
+++ MO1-IntroToPrograming/S01-IntroToPrograming/P06-SquareOfNStars-45d67aa6.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int size = 5; // size of the square
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine(); // move to next line
+        }
+    }
+}
